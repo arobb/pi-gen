3a4f493ccf186e8df67f4c7466e0af2983f18ea7
Andy Robb <andy@andyrobb.com>
2019-09-07 13:46:01 -0700

    Remove content moved to Ansible config

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -55,57 +55,6 @@ EOF
 
 rm -f "${ROOTFS_DIR}/etc/ssh/"ssh_host_*_key*
 
-# on_chroot << EOF
-# patch << 'EOG' /boot/config.txt
-# --- config.txt	2019-01-17 06:23:28.000000000 -0800
-# +++ config copy.txt	2019-01-16 22:56:44.000000000 -0800
-# @@ -11,10 +11,10 @@
-#
-#  # uncomment the following to adjust overscan. Use positive numbers if console
-#  # goes off screen, and negative if there is too much border
-# -#overscan_left=16
-# -#overscan_right=16
-# -#overscan_top=16
-# -#overscan_bottom=16
-# +overscan_left=16
-# +overscan_right=16
-# +overscan_top=16
-# +overscan_bottom=16
-#
-#  # uncomment to force a console size. By default it will be display's size minus
-#  # overscan.
-# EOG
-# EOF
-
-# on_chroot << EOF
-# patch << 'EOG' /boot/config.txt
-# --- /boot/config.txt	2017-08-20 00:44:26.000000000 +0000
-# +++ ./config.txt	2019-08-19 04:36:43.790494373 +0000
-# @@ -43,9 +43,9 @@
-#  #arm_freq=800
-#
-#  # Uncomment some or all of these to enable the optional hardware interfaces
-# -#dtparam=i2c_arm=on
-# -#dtparam=i2s=on
-# -#dtparam=spi=on
-# +dtparam=i2c_arm=on
-# +dtparam=i2s=on
-# +dtparam=spi=on
-#
-#  # Uncomment this to enable the lirc-rpi module
-#  #dtoverlay=lirc-rpi
-# EOG
-# EOF
-
-# on_chroot << EOF
-# # Shim doesn't install as root
-# su pi
-# curl https://raw.githubusercontent.com/arobb/pimoroni-onoffshim-headless/master/onoffshim.sh | bash -s -- -y
-# if [[ "$(whoami)" != "root" ]]; then
-#   exit # Drop su
-# fi
-# EOF
-
 echo "Update CA Certificate package in this environment"
 if [ ! -f /tmp/ca-certs-updated.touch ]; then
   apt update && apt install -y --reinstall ca-certificates
